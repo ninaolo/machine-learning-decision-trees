18d9ec5a1fb0ab199fe9db7e1d2fbdc096226615
Nina <ninaolo@kth.se>
2016-09-05 12:15:53 +0200

    Fix assignment 1 & 2

--- dectrees-py/assignments.py
+++ dectrees-py/assignments.py
@@ -1,24 +1,53 @@
-import monkdata
-import dtree
+import monkdata as m
+import dtree as d
+import drawtree as draw
 
+class TrainingSet:
+    def __init__(self, dataset, name):
+        self.dataset = dataset
+        self.name = name
+
+
+trainingSets = [TrainingSet(m.monk1test, "monk1test"),
+                TrainingSet(m.monk2test, "monk2test"),
+                TrainingSet(m.monk3test, "monk3test")]
+
+""" Assignment 1 """
 def calculateEntropyOfTrainingSets():
-    print("Entropy of monk1test: " + str(dtree.entropy(monkdata.monk1test)))
-    print("Entropy of monk2test: " + str(dtree.entropy(monkdata.monk2test)))
-    print("Entropy of monk3test: " + str(dtree.entropy(monkdata.monk3test)))
+    for set in trainingSets:
+        print("Entropy of " + set.name + ": " + str(d.entropy(set.dataset)))
+    print("")
 
+
+""" Assignment 2 """
 def calculateAverageInformationGainOfTrainingSets():
-    print("Average information gain of monk1test:")
-    for i in range(6):
-        print("a" + str(i+1) + ": " + str(dtree.averageGain(monkdata.monk1test, monkdata.attributes[i])))
-    print("")
+    for set in trainingSets:
+        printInformationGainOfDataset(set.dataset, set.name)
 
-    print("Average information gain of monk2test:")
-    for i in range(6):
-        print("a" + str(i+1) + ": " + str(dtree.averageGain(monkdata.monk2test, monkdata.attributes[i])))
-    print("")
 
-    print("Average information gain of monk3test:")
-    for i in range(6):
-        print("a" + str(i+1) + ": " + str(dtree.averageGain(monkdata.monk3test, monkdata.attributes[i])))
+def splitOnA5AndComputeInformationGainsOfSubsets():
+    a5 = m.attributes[4]
 
-calculateAverageInformationGainOfTrainingSets()
+    for set in trainingSets:
+        for attributeValue in a5.values:
+            subset = d.select(set.dataset, a5, attributeValue)
+            printInformationGainOfDataset(subset, set.name + " splitted on A5 = " + str(attributeValue))
+
+
+def printInformationGainOfDataset(dataset, name):
+    print("\nInformation gain of " + name + ":")
+    for i in range(len(m.attributes)):
+        print(m.attributes[i].name + ": " + str(d.averageGain(dataset, m.attributes[i])))
+    print("Best attribute is: " + str(d.bestAttribute(dataset, m.attributes)))
+
+def drawMonk1():
+    tree = d.buildTree(m.monk1test, m.attributes)
+    draw.drawTree(tree)
+
+def drawMonk2():
+    tree = d.buildTree(m.monk2test, m.attributes)
+    draw.drawTree(tree)
+
+""" Assignment 3 """
+def buildTreesAndComputePerformance():
+    pass
